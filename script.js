9330c29a7a04730cb0d18be520f595d7b57f0a46
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-26 12:46:43 -0500

    added main functionality to app getting location and changing weather to that location. Giving the ability for user to look up weather at certain location.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+
 const generateCityAPIParams = (textInput) => {
     // Replacing all spaces with %20, the symbol for spaces in a query string
-    parsedTextInput = textInput.replace(/ /g, "%20")
+    let parsedTextInput = textInput.replace(/ /g, "%20")
+    parsedTextInput = parsedTextInput.replace(/ /g, "%2C")
     const url = `https://andruxnet-world-cities-v1.p.rapidapi.com/?query=${parsedTextInput}&searchby=city`;
     const options = {
         method: 'GET',
@@ -15,7 +18,7 @@ const generateCityAPIParams = (textInput) => {
 
 const getCityData = async (APIParams) => {
     try {  
-        console.log(APIParams)
+        //console.log(APIParams)
         const response = await fetch(APIParams[0], APIParams[1]);
         const result = await response.json();
         return result;
@@ -26,30 +29,9 @@ const getCityData = async (APIParams) => {
 
 getCityData().then(cities => {
     cities.forEach(city => {
-        console.log(city.city, city.state)
+        //console.log(city.city, city.state)
     })
 });
-
-// creates city set to eliminate duplicate cities in the city list
-const createCitySet = (cities) => {
-    const filteredCities = new Array();
-    filteredCities.push(cities[0]);
-    for(let i = 1; i < cities.length; i++) {
-        // Set limit to make only 5 elements in the filteredCities array
-        let indexLimit = 0;
-        for(let j = 0; j < filteredCities.length; j++) {
-            if (cities[i].city === filteredCities[j].city && cities[i].state === filteredCities[j].state && cities[i].country === filteredCities[j].country) {
-                continue;
-            } else if (j === filteredCities.length - 1 && indexLimit < 5) {
-                filteredCities.push(cities[i]);
-                indexLimit++;
-            } else {
-                break;
-            }
-        }
-    }
-    return filteredCities;
-}
 
 // get inputbox element that user types city in
 const citySearchBox = document.getElementById("city-search-box");
@@ -63,23 +45,116 @@ citySearchBox.addEventListener("input", (event) => {
     const textInput = event.target.value;
     if(textInput.length >= 3) {
         const cityAPIParams = generateCityAPIParams(textInput)
-        getCityData(cityAPIParams).then(cities => {
-            const citySet = createCitySet(cities);
+        getCityData(cityAPIParams).then(citySet => {
+            // const citySet = createCitySet(cities);
             // Get only 5 results
-            if(citySet.length > 5) {
-                for(let i = 0; i < 5; i++) {
-                    let newCity = document.createElement("option");
-                    newCity.setAttribute("value", citySet[i].city + ", " + citySet[i].state);
-                    cityList.appendChild(newCity);
-                }
-            } else {
-                for(let i = 0; i < citySet.length; i++) {
-                    let newCity = document.createElement("option");
-                    newCity.setAttribute("value", citySet[i].city + ", " + citySet[i].state);
-                    cityList.appendChild(newCity);
-                }
+            for(let i = 0; i < citySet.length; i++) {
+                let newCity = document.createElement("option");
+                newCity.setAttribute("value", citySet[i].city + ", " + citySet[i].state + ", " + citySet[i].country);
+                cityList.appendChild(newCity);
             }
-            
         })
     }
 })
+
+// Function for fetching geospatial codes (lat, long)
+const getCoordinates = async (cityArr) => {
+    const fetchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityArr[0]},${cityArr[1]}&limit=1&appid=561be9233a319bd7b94859e59933426b`;
+    const response = await fetch(fetchUrl);
+    const cityData = await response.json();
+    return [cityData[0].lat, cityData[0].lon];
+}
+
+// Function for fetching the weather data with the coordinates given
+const getWeatherData = async (lat, long) => {
+    const fetchUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&appid=561be9233a319bd7b94859e59933426b`;
+    const response = await fetch(fetchUrl);
+    const weatherData = await response.json();
+    //console.log(weatherData);
+    return [weatherData.current.temp, weatherData.current.humidity, weatherData.current.wind_speed, weatherData.current.weather[0].main];
+}
+
+// Make event listener for search button
+// 1. get the element in the html for the search button
+const searchButton = document.getElementById("search-button");
+
+const processWeatherByCity = (event) => {
+    const searchCity = citySearchBox.value;
+    const cityArray = searchCity.split(", ", 2);
+    getCoordinates(cityArray).then(coordinates => {
+        const [lat, long] = coordinates;
+        getWeatherData(lat, long).then(weatherData => {
+            const [temp, humidity, wind_speed, weatherDescription] = weatherData;
+            //console.log(temp, humidity, wind_speed, weatherDescription.toLowerCase());
+            // Create the elements in the HTML that need to be dynamic
+            const weatherImage = document.getElementById("weather-img");
+            const tempElement = document.getElementById("temp");
+            const cityElement = document.getElementById("city");
+            const humidityElement = document.getElementById("humidity");
+            const windElement = document.getElementById("wind");
+            // Change the elements to reflect the current weather
+            weatherImage.setAttribute("src", `images/${weatherDescription.toLowerCase()}.png`);
+            tempElement.innerText = `${Math.trunc(temp)}°F`;
+            cityElement.innerText = `${cityArray.join(", ")}`;
+            humidityElement.innerText = `${Math.trunc(humidity)}%`;
+            windElement.innerText = `${Math.trunc(wind_speed)} mph`;
+        });
+    })
+}
+
+// add event listener
+searchButton.addEventListener("click", processWeatherByCity);
+
+getCoordinates(['Cincinnati', 'Ohio']).then(data => {
+    // console.log(data);
+});
+
+const getCityName = (latitude, longitude) => {
+    // console.log(latitude, longitude)
+    const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        return data
+      })
+      .catch(error => console.error('Error:', error));
+}
+
+if ("geolocation" in navigator) {
+  /* geolocation is available */
+  navigator.geolocation.getCurrentPosition(function(position) {
+    getWeatherData(position.coords.latitude, position.coords.longitude).then(weatherData => {
+        
+        const [temp, humidity, wind_speed, weatherDescription] = weatherData;
+        // console.log(temp, humidity, wind_speed, weatherDescription.toLowerCase());
+        // Create the elements in the HTML that need to be dynamic
+        const weatherImage = document.getElementById("weather-img");
+        const tempElement = document.getElementById("temp");
+        const cityElement = document.getElementById("city");
+        const geoUrl = `https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+        fetch(geoUrl)
+        .then(response => response.json())
+        .then(data => {
+            const cityName = (data.features[0].properties.address.city || data.features[0].properties.address.town || data.features[0].properties.address.village || data.features[0].properties.address.municipality);
+            const stateName = data.features[0].properties.address.state;
+            cityElement.innerText = `${cityName}, ${stateName}`;
+        })
+        .catch(error => console.error('Error:', error));
+        
+        const humidityElement = document.getElementById("humidity");
+        const windElement = document.getElementById("wind");
+        // Change the elements to reflect the current weather
+        weatherImage.setAttribute("src", `images/${weatherDescription.toLowerCase()}.png`);
+        tempElement.innerText = `${Math.trunc(temp)}°F`;
+        
+        humidityElement.innerText = `${Math.trunc(humidity)}%`;
+        windElement.innerText = `${Math.trunc(wind_speed)} mph`;
+    })
+  }, function(error) {
+    console.error("Error Code = " + error.code + " - " + error.message);
+  });
+} else {
+  /* geolocation IS NOT available */
+  console.log("Geolocation is not supported by this browser.");
+}
